173afd12ba6c97cfd7fd80623a395b35e0bb2c65
selincaglarr <69199076+selincaglarr@users.noreply.github.com>
2020-10-10 14:50:14 +0300

    Update addDigits.js

--- addDigits.js
+++ addDigits.js
@@ -5,3 +5,18 @@ let sayiyicevir =num=>[ ...num.toString() ].map( s => Number(s) ).reduce((x,y)=>
     return sayiyicevir(sayiyicevir(num))
 
 };
+
+
+const addDigits = function(num) {
+let ayir= num=>{
+let yenisayi= num.toString().split("").map(x=>Number(x)).reduce((x,y)=>x+y)
+        if(yenisayi>9){
+            ayir(yenisayi)
+        }
+         else {
+             return console.log(yenisayi)
+        }
+}
+    ayir(num)
+};
+addDigits(39)
